80ca64d757805873284be2e1f6106f66c664e4d4
Charlie Wadds <charliewadds@me.com>
2024-06-12 16:06:09 -0400

    update outputs

--- PythonTesting/CsvCompare.py
+++ PythonTesting/CsvCompare.py
@@ -19,15 +19,22 @@ def compare_csv(file1, file2):
     m1 = np.loadtxt(file1, dtype=np.double, delimiter=",")
     m2 = scipy.io.loadmat(file2)
 
+    err = np.zeros(m1.shape)
+    absErr = np.zeros(m1.shape)
 
-    diff = np.sum(np.subtract(m1, m2['C_des']))
+    maxAbsErr = 0
+    for i in range(m1.shape[0]):
+        for j in range(m1.shape[1]):
+            err[i, j] = m1[i, j] - m2['C_des'][i, j]
+            absErr[i, j] = abs(err[i, j])
+            if(absErr[i, j] > maxAbsErr):
+                maxAbsErr = absErr[i, j]
 
-    print(diff)
-    print("M1")
-    print(m1)
-    print("M2")
-    print(m2['C_des'])
+    print("Max abs error: ", maxAbsErr)
+    print("Mean abs error: ", np.mean(absErr))
 
+    np.savetxt("./err.csv", err, delimiter=",", fmt='%.15f')
+    np.savetxt("./absErr.csv", absErr, delimiter=",", fmt='%.15f')
     np.savetxt("./matlab2.csv", m2['C_des'], delimiter=",", fmt='%.15f')
 
 compare_csv('cmake-build-debug/Control_good.csv', 'ControlSim2.mat')
